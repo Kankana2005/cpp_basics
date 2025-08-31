ec8213ac9eb43e0da6025a7d2eea9386642b1c27
Kankana2005 <kankanaghosh129@gmail.com>
2025-08-31 17:20:27 +0530

    solved recursion problems

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -1,23 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print_n_times(int n){
-    int cnt=0;
-    if(cnt==n){
+void print_n_times(int i , int n){
+    if(i>n)
         return;
+    cout<<"hello"<<endl;
+    print_n_times(i+1,n);
     }
-    else{
-        cout<<"hello"<<endl;
-        cnt++;
 
-    }
-    print_n_times(n-1);
+void print_one_to_n(int i,int n){
+    if(i>n)
+        return;
+    cout<<i<<" ";
+    print_one_to_n(i+1,n);    
+}
+
+void print_n_to_one(int i,int n){
+    if(i<1)
+        return;
+    cout<<i<<" ";
+    print_n_to_one(i-1,n);    
+}
+
+void print_1ton_BackTracking(int i , int n){
+    if(i<1)
+        return;
+    print_1ton_BackTracking(i-1,n);
+    cout<<i<<" ";
+}
+
+void print_nto1_BackTracking(int i , int n){
+    if(i>n)
+        return;
+    print_nto1_BackTracking(i+1,n);
+    cout<<i<<" ";
 }
 
 int main(){
     int n;
-    cout<<"Enter the number";
+    cout<<"Enter the number: ";
     cin>>n;
-    print_n_times(n);
+    // print_n_times(1,n);
+    // print_one_to_n(1,n);
+    // print_n_to_one(n,n);
+    // print_1ton_BackTracking(n,n);
+    // print_nto1_BackTracking(1,n);
     return 0;
 }
