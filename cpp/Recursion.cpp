d5d977fd9c0e5f912f7ea7219fa81835a98c5ede
Kankana2005 <kankanaghosh129@gmail.com>
2025-09-03 01:28:52 +0530

    solved

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -106,6 +106,27 @@ void print_all_subsequences(int ind,vector<int> &ds , int arr[],int n){
 
 }
 
+void print_subsequence_whoseSum_K(int ind,vector<int>&ds,int s ,int sum,int arr[],int n){
+    if(ind == n){
+        if(s == sum){
+        for(auto it: ds) cout<<it<<" ";
+        cout<<endl;
+    }
+    return;
+    }
+ds.push_back(arr[ind]);
+s += arr[ind];
+
+print_subsequence_whoseSum_K(ind+1,ds,s,sum,arr,n);
+
+s -= arr[ind];
+
+ds.pop_back();
+
+print_subsequence_whoseSum_K(ind+1,ds,s,sum,arr,n);
+
+}
+
 int main(){
     // int n;
     // string n;
@@ -145,13 +166,17 @@ int main(){
      // cout<<fib(n);
 
     //printing all the subsequences:
-    int arr[] = {3,1,2};
+    // int arr[] = {3,1,2};
+    // int n = 3;
+    // vector<int>ds;
+    // print_all_subsequences(0,ds,arr,n);
+
+    //Printing Subsequences whose sum is k
+    int arr[] = {1,2,1};
     int n = 3;
+    int sum = 2;
     vector<int>ds;
-    print_all_subsequences(0,ds,arr,n);
-
-
-
+    print_subsequence_whoseSum_K(0,ds,0,sum,arr,n);
 
     return 0;
 }
